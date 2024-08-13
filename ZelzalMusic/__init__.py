861992243984888fe700e6c1420025002d34f13b
Hussien <158073869+Hjwad@users.noreply.github.com>
2024-08-13 19:22:57 +0300

    Update __init__.py

--- ZelzalMusic/__init__.py
+++ ZelzalMusic/__init__.py
@@ -4,12 +4,14 @@
 
 from ZelzalMusic.core.bot import Zelzaly
 from ZelzalMusic.core.dir import dirr
+from ZelzalMusic.core.git import git
 from ZelzalMusic.core.userbot import Userbot
 from ZelzalMusic.misc import dbb, heroku
 
 from .logging import LOGGER
 
 dirr()
+git()
 dbb()
 heroku()
 
